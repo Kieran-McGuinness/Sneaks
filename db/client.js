853acfbf6dbab515150c2cf0cc7e5699227e65ec
Kieran <zumaht@gmail.com>
2022-05-12 17:02:48 -0400

    edits after project completion

--- db/client.js
+++ db/client.js
@@ -2,7 +2,7 @@
 const { Client } = require("pg");
 
 // change the DB_NAME string to whatever your group decides on
-const DB_NAME = "Shopper";
+const DB_NAME = "shopper";
 
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
