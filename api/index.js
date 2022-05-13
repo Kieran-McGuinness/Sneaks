5e8dc46766011444c6d11189bdb7f13053601ea8
Kieran McGuinness <zumaht@gmail.com>
2022-05-13 17:31:21 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,48 +8,16 @@ const usersRouter = require("./user");
 const adminRouter = require("./admin");
 const itemsRouter = require("./items");
 const shoppingCartRouter = require("./shopping_cart");
-
-// const { REACT_APP_JWT_SECRET } = process.env
-// const jwt = require("jsonwebtoken");
 const { getUserById } = require("../db");
 
-// apiRouter.use(async (req, res, next) => {
-//   const prefix = 'Bearer ';
-//   const auth = req.header('Authorization');
 
-//   if (!auth) {
-//       next();
-//   } else if (auth.startsWith(prefix)) {
-//       const token = auth.slice(prefix.length);
-//       try {
-//           const { id } = jwt.verify(token, REACT_APP_JWT_SECRET);
-
-//           if (id) {
-//               req.user = await getUserById(id);
-//               next()
-//           }
-//       } catch ({ name, message }) {
-//           next({ name, message });
-//       }
-//   } else {
-//       next({
-//           name: 'AuthorizationHeaderError',
-//           message: `Authorization token must start with bearer`
-//       });
-//   }
-// });
 
 apiRouter.get("/", (req, res, next) => {
   res.send({
-    message: "API is under construction!",
+    message: "API is Working!",
   });
 });
 
-apiRouter.get("/health", (req, res, next) => {
-  res.send({
-    healthy: true,
-  });
-});
 
 // place your routers here
 
