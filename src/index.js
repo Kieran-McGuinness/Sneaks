88eeae38eee7b8c6eed7ccc375de77abcec24173
Kieran <zumaht@gmail.com>
2022-07-21 19:14:33 -0400

    added webpack config

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import { App } from "./components"
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
 import "./style/index.css";
-require("dotenv").config();
+// require("dotenv").config();
+
 ReactDOM.render(<App />, document.getElementById("root"));
