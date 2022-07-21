9ef9581fdcd5de077179fc9e2bd0f33d1b8d85a0
Kieran McGuinness <zumaht@gmail.com>
2022-07-21 18:29:51 -0400

    Update Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,108 +1,108 @@
-import React, { useState, useEffect } from "react";
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import Box from "@mui/material/Box";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
-import Collapse from "@mui/material/Collapse";
+// import React, { useState, useEffect } from "react";
+// import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+// import Box from "@mui/material/Box";
+// import Snackbar from "@mui/material/Snackbar";
+// import MuiAlert from "@mui/material/Alert";
+// import Collapse from "@mui/material/Collapse";
 
-const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
-  const [cardMessage, setCardMessage] = useState("");
-  const [severity, setSeverity] = useState("");
-  const [open, setOpen] = useState(false);
+// const CheckoutForm = () => {
+//   const stripe = useStripe();
+//   const elements = useElements();
+//   const [cardMessage, setCardMessage] = useState("");
+//   const [severity, setSeverity] = useState("");
+//   const [open, setOpen] = useState(false);
 
-  const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
-  const handleClick = () => {
-    setOpen(true);
-  };
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
+//   const Alert = React.forwardRef(function Alert(props, ref) {
+//     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+//   });
+//   const handleClick = () => {
+//     setOpen(true);
+//   };
+//   const handleClose = (event, reason) => {
+//     if (reason === "clickaway") {
+//       return;
+//     }
 
-    setOpen(false);
-  };
+//     setOpen(false);
+//   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
 
-    if (!stripe || !elements) {
-      // Stripe.js has not loaded yet. Make sure to disable
-      // form submission until Stripe.js has loaded.
-      return;
-    }
+//     if (!stripe || !elements) {
+//       // Stripe.js has not loaded yet. Make sure to disable
+//       // form submission until Stripe.js has loaded.
+//       return;
+//     }
 
-    // Get a reference to a mounted CardElement. Elements knows how
-    // to find your CardElement because there can only ever be one of
-    // each type of element.
-    const card = elements.getElement(CardElement);
+//     // Get a reference to a mounted CardElement. Elements knows how
+//     // to find your CardElement because there can only ever be one of
+//     // each type of element.
+//     const card = elements.getElement(CardElement);
 
-    if (card == null) {
-      return;
-    }
+//     if (card == null) {
+//       return;
+//     }
 
-    // Use your card Element with other Stripe.js APIs
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card,
-    });
+//     // Use your card Element with other Stripe.js APIs
+//     const { error, paymentMethod } = await stripe.createPaymentMethod({
+//       type: "card",
+//       card,
+//     });
 
-    if (error) {
-      console.log("[error]", error);
-    } else {
-      console.log("[PaymentMethod]", paymentMethod);
-      setSeverity("success");
-      setCardMessage("Payment Processed!");
-      handleClick();
-    }
-  };
+//     if (error) {
+//       console.log("[error]", error);
+//     } else {
+//       console.log("[PaymentMethod]", paymentMethod);
+//       setSeverity("success");
+//       setCardMessage("Payment Processed!");
+//       handleClick();
+//     }
+//   };
 
-  return (
-    <Box
-      component="form"
-      id="checkout-form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-      onSubmit={handleSubmit}
-    >
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
-              },
-            },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
-        <Collapse in={open}>
-          <Alert
-            onClose={handleClose}
-            severity={severity}
-            sx={{ width: "100%" }}
-          >
-            {cardMessage}
-          </Alert>
-        </Collapse>
-      </Snackbar>
-    </Box>
-  );
-};
+//   return (
+//     <Box
+//       component="form"
+//       id="checkout-form"
+//       sx={{
+//         "& .MuiTextField-root": { m: 1, width: "25ch" },
+//       }}
+//       noValidate
+//       autoComplete="off"
+//       onSubmit={handleSubmit}
+//     >
+//       <CardElement
+//         options={{
+//           style: {
+//             base: {
+//               fontSize: "16px",
+//               color: "#424770",
+//               "::placeholder": {
+//                 color: "#aab7c4",
+//               },
+//             },
+//             invalid: {
+//               color: "#9e2146",
+//             },
+//           },
+//         }}
+//       />
+//       <button type="submit" disabled={!stripe}>
+//         Pay
+//       </button>
+//       <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+//         <Collapse in={open}>
+//           <Alert
+//             onClose={handleClose}
+//             severity={severity}
+//             sx={{ width: "100%" }}
+//           >
+//             {cardMessage}
+//           </Alert>
+//         </Collapse>
+//       </Snackbar>
+//     </Box>
+//   );
+// };
 
-export default CheckoutForm;
+// export default CheckoutForm;
